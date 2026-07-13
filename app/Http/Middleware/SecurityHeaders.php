34144eb9a0cda75fe4a0418eaf039bf08119dd5d
dok4 <girlsfrontline45@gmail.com>
2026-07-13 20:27:04 +0800

    修复生产环境 CSP 禁用 unsafe-eval 导致前端 JS 报错

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -41,7 +41,7 @@ class SecurityHeaders
         } else {
             $csp = implode('; ', [
                 "default-src 'self'",
-                "script-src 'self' 'unsafe-inline'",
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                 "font-src 'self' data: https://fonts.gstatic.com",
                 "img-src 'self' data: blob: https:",
