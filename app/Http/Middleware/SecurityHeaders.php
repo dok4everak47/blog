87f3c7503ad44a69c54a384d315554f2c156939e
dok4 <girlsfrontline45@gmail.com>
2026-07-13 20:58:08 +0800

    CSP: connect-src 添加 blob: 允许 cropper 裁剪图片

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -45,7 +45,7 @@ class SecurityHeaders
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                 "font-src 'self' data: https://fonts.gstatic.com",
                 "img-src 'self' data: blob: https:",
-                "connect-src 'self'",
+                "connect-src 'self' blob:",
                 "media-src 'self'",
                 "object-src 'none'",
                 "base-uri 'self'",
