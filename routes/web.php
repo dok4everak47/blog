ec29e87e75a74ce740ec3891e06be462b50f1f72
dok4ever <luongchinc@gmail.com>
2026-07-31 21:07:45 +0800

    fix: remove duplicate comments resource route

--- routes/web.php
+++ routes/web.php
@@ -130,4 +130,3 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 Route::resource('tags', TagController::class);
-Route::resource('comments', CommentController::class);
